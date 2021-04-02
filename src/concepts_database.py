1639fcdcd7300034c13b1144820c053b9c3b28d1
Lucas Pastor <lucaspastorduran@gmail.com>
2021-04-02 21:10:03 +0200

    Add AddConcept into saved_concepts database

--- src/concepts_database.py
+++ src/concepts_database.py
@@ -4,13 +4,22 @@ import pandas as pd
 
 class ConceptElement:
     # class static variables (shared among all instances)
-    _AVAILABLE_FIELDS = ["Concept Name", "Alias", "Category"]
+    _NAME_FIELD = "Concept Name"
+    _ALIAS_FIELD = "Alias"
+    _CATEGORY_FIELD = "Category"
 
     def __init__(self, name, alias="", category=""):
         # class instance attributes
         self._name = name
         self._alias = alias
         self._category = category
+
+    def is_in_database(self, saved_concepts_df):
+        # Check if already there is a row with same "Concept Name"
+        return self._name in saved_concepts_df[self._NAME_FIELD]
+
+    def convert_to_dict(self):
+        return {self._NAME_FIELD : self._name; _ALIAS_FIELD : self._alias; self._CATEGORY_FIELD : self._category}
 
     def __str__(self):
         message = f"Concept:\nName: {self._name}. Alias: {self._alias}. Category: {self._category}"
@@ -20,7 +29,7 @@ class ConceptsDatabase:
     # class static variables (shared among all instances)
     _DEFAULT_DATABASE_PATH = "/home/lucas/Documents/Cursos/expenses-application/src/saved_concepts.csv"
     _DEFAULT_DELIMITER = ";"
-    _CONCEPT_FIELDS
+    _CONCEPT_FIELDS = {"name":ConceptElement._NAME_FIELD ;"alias":ConceptElement._ALIAS_FIELD; "category":ConceptElement._CATEGORY_FIELD}
 
     def __init__(self):
         # class instance attributes
@@ -33,8 +42,10 @@ class ConceptsDatabase:
 
     def add_concept(self, concept_element):
         # Check if concept already exists
-        # Add concept if is not already
-        return None
+        if not concept_element.is_in_database(self._saved_concepts):
+            # Add concept if is not already
+            concept_dict = concept_element.convert_to_dict()
+            self._saved_concepts.append(concept_dict)
 
     def __str__(self):
         message = f"""
